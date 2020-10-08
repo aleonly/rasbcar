c06f9fc346dc664188324b57079cadeb38c65629
aleonly <aleonly23@gmail.com>
2020-10-08 17:37:11 +0800

    updae rasp-camero

--- demo/rasp-camera.py
+++ demo/rasp-camera.py
@@ -2,20 +2,20 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
- 
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Flip camera vertically
-    frame = cv2.flip(frame, -1)
+    # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+
     # Display the resulting frame
+    cv2.imshow('frame', frame)
     cv2.imshow('gray', gray)
-    if cv2.waitKey(0) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# release the capture
+# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
